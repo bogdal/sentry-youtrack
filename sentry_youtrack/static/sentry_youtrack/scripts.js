2284d6dc0c453510d109dec71af430fd2468ddb0
Adam Bogdał <adam@bogdal.pl>
2016-03-16 21:58:43 +0100

    Fix of saving the default values

--- sentry_youtrack/static/sentry_youtrack/scripts.js
+++ sentry_youtrack/static/sentry_youtrack/scripts.js
@@ -9,6 +9,22 @@ function save_as_default(el, field, value) {
             value: value
         },
         beforeSend: function( xhr ) {
+            function getCookie(name) {
+                var cookieValue = null;
+                if (document.cookie && document.cookie != '') {
+                    var cookies = document.cookie.split(';');
+                    for (var i = 0; i < cookies.length; i++) {
+                        var cookie = jQuery.trim(cookies[i]);
+                        // Does this cookie string begin with the name we want?
+                        if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                            cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                            break;
+                        }
+                    }
+                }
+                return cookieValue;
+            }
+            xhr.setRequestHeader("X-CSRFToken", getCookie('csrf'));
             el.fadeOut(200);
         }
     }).done(function(data){
