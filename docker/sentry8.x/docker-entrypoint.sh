b249281ef9e729cde9b24fdb29197ef24ec99ba2
Adam Bogdał <adam@bogdal.pl>
2016-03-16 08:16:17 +0100

    Add custom plugin configuration

--- docker/sentry8.x/docker-entrypoint.sh
+++ docker/sentry8.x/docker-entrypoint.sh
@@ -2,6 +2,14 @@
 
 set -e
 
+default_settings='/etc/sentry/sentry.conf.py'
+custom_settings='/plugin/docker/sentry8.x/custom.conf.py'
+if [ -f "$custom_settings" ]; then
+    line="execfile('$custom_settings')"
+	if ! grep -q "$line" "$default_settings"; then
+		echo "$line" >> "$default_settings"
+	fi
+fi
 if [ -d '/plugin' ]; then
     cd /plugin && python setup.py develop --user
 fi
