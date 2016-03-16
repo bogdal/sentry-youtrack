f70c34c6a1125c8f12d341949394a5d32c79585e
Adam Bogdał <adam@bogdal.pl>
2016-03-16 08:41:22 +0100

    Get rid of old sentry container

--- docker/sentry/docker-entrypoint.sh
+++ docker/sentry/docker-entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 
 default_settings='/etc/sentry/sentry.conf.py'
-custom_settings='/plugin/docker/sentry8.x/custom.conf.py'
+custom_settings='/plugin/docker/sentry/custom.conf.py'
 if [ -f "$custom_settings" ]; then
     line="execfile('$custom_settings')"
 	if ! grep -q "$line" "$default_settings"; then
